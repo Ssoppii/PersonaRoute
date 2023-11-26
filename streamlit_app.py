9f0f09fce2ba9b161803a5df4925ccad5087814a
Ssoppii <jihaenina@snu.ac.kr>
2023-11-26 15:42:48 +0900

    231126 15:42 test commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
+# title
+st.title('Persona Route - Jeju Island')
+
+# header
 st.write('Hello, *World!* :sunglasses:')
+
+st.text_input("Write your favorite music title.", max_chars=None, key=None, type="default", label_visibility="visible")
